8a4cf8861120829af7649bd67d1b00578355832b
Juan Casanueva <juancasanueva@gmail.com>
2023-02-26 14:35:31 +0100

    deploy the appache webserver on the Azure Virtual machine and configure it with Ansible

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -1,9 +1,9 @@
 # K8 cluster
 #resource "azurerm_kubernetes_cluster" "k8" {
-#  name                = "aks1"
+#  name                = "akscp2"
 #  location            = azurerm_resource_group.rg.location
 #  resource_group_name = azurerm_resource_group.rg.name
-#  dns_prefix          = "aks1"
+#  dns_prefix          = "akscp2"
 #
 #  default_node_pool {
 #    name       = "default"
@@ -11,9 +11,26 @@
 #    vm_size    = "Standard_D2_v2"
 #  }
 #
+#  linux_profile {
+#    admin_username = var.ssh_user
+#
+#    ssh_key {
+#      key_data = file(var.ssh_public_key)
+#    }
+#  }
+#
 # identity {
 #    type = "SystemAssigned"
 #  }
 #  
 #  depends_on = [azurerm_container_registry.acr]
+#}
+
+#resource "azurerm_managed_disk" "disk" {
+#  name                 = "disk"
+#  location             = azurerm_resource_group.rg.location
+#  resource_group_name  = azurerm_resource_group.rg.name
+#  storage_account_type = "Standard_LRS"
+#  create_option        = "Empty"
+#  disk_size_gb         = "1"
 #}
