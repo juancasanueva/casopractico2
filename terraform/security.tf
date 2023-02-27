a0b4a5c48f6893019d85a8ec0b29e20904119e3d
Juan Casanueva <juancasanueva@gmail.com>
2023-02-27 18:20:54 +0100

    kubernetes yaml file with mongodb env variables

--- terraform/security.tf
+++ terraform/security.tf
@@ -15,25 +15,21 @@ resource "azurerm_network_security_group" "secgroup" {
     source_address_prefix       = "*"
     destination_address_prefix  = "*"
   }
+  security_rule {  
+    name                        = "http"
+    priority                    = 1002
+    direction                   = "Inbound"
+    access                      = "Allow"
+    protocol                    = "Tcp"
+    source_port_range           = "*"
+    destination_port_range      = "8080"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+  }
 }
 
 # Association of the NEtwork Security Group to the Subnet
 resource "azurerm_subnet_network_security_group_association" "nsg-link" {
   subnet_id                 = azurerm_subnet.subnet.id
   network_security_group_id = azurerm_network_security_group.secgroup.id
-}
-
-# The Http rule to access port 8080
-resource "azurerm_network_security_rule" "http" {
-  name                        = "http"
-  priority                    = 1002
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "8080"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.rg.name
-  network_security_group_name = azurerm_network_security_group.secgroup.name
 }
