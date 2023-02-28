3e926e3946b9627cd19eecee4eee5bb987808984
Juan Casanueva <juancasanueva@gmail.com>
2023-02-28 19:40:13 +0100

    encrypt acr password variable with ansible vault

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,3 +1,3 @@
-ansible-playbook -i inventory 01_playbook.yml
-ansible-playbook -i inventory 02_playbook.yml
+ansible-playbook --ask-vault-pass -i inventory 01_playbook.yml
+ansible-playbook --ask-vault-pass -i inventory 02_playbook.yml
 ansible-playbook 03_playbook.yml
